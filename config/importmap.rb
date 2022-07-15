9cb2e2fcc1254cb1e584d34383e0466039c49b90
zakaahmad1561 <48981988+zakaahmad1561@users.noreply.github.com>
2022-07-15 18:48:22 +0500

    add active storage and action text

--- config/importmap.rb
+++ config/importmap.rb
@@ -13,3 +13,5 @@ pin "@fortawesome/free-solid-svg-icons", to: "https://ga.jspm.io/npm:@fortawesom
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.1.3/dist/js/bootstrap.esm.js"
 pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.5/lib/index.js"
 pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.0/dist/jquery.js"
+pin "trix"
+pin "@rails/actiontext", to: "actiontext.js"

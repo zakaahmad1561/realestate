f01ed640130023c1c2ed7056fced523f6ae6d37a
zakaahmad1561 <48981988+zakaahmad1561@users.noreply.github.com>
2022-07-17 19:59:10 +0500

    pictures has been added to articles

--- Gemfile
+++ Gemfile
@@ -73,3 +73,5 @@ end
 
 gem 'bootstrap', '~> 5.1', '>= 5.1.3'
 gem "dotenv-rails"
+gem 'cloudinary'
+gem 'byebug'
--- app/helpers/attachments_helper.rb
+++ app/helpers/attachments_helper.rb
@@ -1,2 +1,22 @@
 module AttachmentsHelper
+    def check_box_helper(pic)
+        check_box_tag "files_ids[]", pic.id, false, class: 'selectable'
+        
+    end
+
+    def user_image_helper(pic)
+        if pic.image?
+            cl_image_tag((Cloudinary::Utils.private_download_url(pic.key, 'jpg')), width: 300, height: 250, gravity: :auto, crop: :thumb, fetch_format: :auto)
+        end
+    end
+
+    def user_video_helper(pic)
+        if pic.video?
+            video_tag pic.url,controls: true
+        end
+    end
+
+    def user_documents_helper(pic)
+        link_to pic.url, rails_blob_path(pic, disposition: "attachment") 
+    end
 end

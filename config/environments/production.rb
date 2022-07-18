cfe06a2372729ebedd5332e8877fc19b443461d5
zakaahmad1561 <48981988+zakaahmad1561@users.noreply.github.com>
2022-07-18 13:19:24 +0500

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -113,5 +113,5 @@ Rails.application.configure do
     :authentication => :plain,
     :enable_starttls_auto => true
   }  
-  # config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = true
 end

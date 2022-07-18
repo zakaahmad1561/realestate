690d20561178bde4236b08263bd128f2b6f1cfe4
zakaahmad1561 <48981988+zakaahmad1561@users.noreply.github.com>
2022-07-18 12:59:38 +0500

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,9 +91,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.perform_deliveries = true
-  # config.action_mailer.default_url_options = { 
-  #   host: 'https://khizarhayat.herokuapp.com/' 
-  # }
+  config.action_mailer.default_url_options = { 
+    host: 'https://khizarhayat.herokuapp.com/' 
+  }
   # config.action_mailer.delivery_method = :smtp
   # config.action_mailer.smtp_settings = {
   #   user_name:      "zakaahmad1561@gmail.com",
